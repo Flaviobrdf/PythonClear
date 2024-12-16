f4a271aa20050d8271aac014070bb0d46fc9049a
Flaviobrdf <flaviobrdf@hotmail.com>
2024-12-16 00:41:59 -0300

    update2

--- atividade4.py
+++ atividade4.py
@@ -1,6 +1,15 @@
+from datetime import datetime
 import os
 import csv
 import re
+
+import csv
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
+from reportlab.lib import colors
+from reportlab.lib.units import inch
+from reportlab.platypus import SimpleDocTemplate, Table, TableStyle
+
 
 arquivo = "cadastro.csv"
 
@@ -10,7 +19,9 @@ def menu():
     print("2) Listar")
     print("3) Atualizar")
     print("4) Deletar")
-    print("5) Sair")
+    print("5) Buscar por ID")
+    print("6) Exportar Cadastros em PDF")
+    print("7) Sair")
 
 def cadastrar():
     if not os.path.isfile(arquivo):
@@ -79,7 +90,7 @@ def deletar():
 def main():
     while True:
         menu()
-        opcao = input("Opção (1-5): ")
+        opcao = input("Opção (1-7): ")
 
         if opcao == "1":
             cadastrar()
@@ -90,6 +101,10 @@ def main():
         elif opcao == "4":
             deletar()
         elif opcao == "5":
+            idBuscar()
+        elif opcao == "6":
+            exportar_para_pdf()
+        elif opcao == "7":
             print("Fim do Programa")
             break
         else:
@@ -129,6 +144,64 @@ def funcCad ():
                 break
             
     listar()
+
+
+def idBuscar():
+        arquivo = 'cadastro.csv'  
+        id_escolhido = int(input("Digite o ID que deseja buscar: "))
+        linha_encontrada = None
+        
+        with open(arquivo, mode='r', newline='', encoding='utf-8') as arquivo_csv:
+            leitor = csv.reader(arquivo_csv)
+            for id, linha in enumerate(leitor, start=1):
+                if id == id_escolhido:
+                    linha_encontrada = linha
+                    break
+        
+        if linha_encontrada:
+            print(f"Linha encontrada com ID {id_escolhido}: {linha_encontrada}")
+        else:
+            print(f"ID {id_escolhido} não encontrado no arquivo.")
+
+def ler_dados_csv(arquivo_csv):
+    dados = []
+    with open(arquivo_csv, mode='r', newline='', encoding='utf-8') as file:
+        leitor = csv.reader(file)
+        dados = list(leitor)
+    return dados
+
+
+def exportar(arquivo_pdf, dados):
+
+        doc = SimpleDocTemplate(arquivo_pdf, pagesize=letter)
+        elementos = []
+
+        tabela = Table(dados)
+        estilo = TableStyle([
+            ('BACKGROUND', (0, 0), (-1, 0), colors.grey),
+            ('TEXTCOLOR', (0, 0), (-1, 0), colors.whitesmoke),
+            ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
+            ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
+            ('FONTSIZE', (0, 0), (-1, 0), 14),
+            ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
+            ('BACKGROUND', (0, 1), (-1, -1), colors.beige),
+            ('GRID', (0, 0), (-1, -1), 1, colors.black)
+        ])
+
+        tabela.setStyle(estilo)
+
+        elementos.append(tabela)
+        
+        doc.build(elementos)
+
+
+def exportar_para_pdf(): 
+
+    dados = ler_dados_csv('cadastro.csv') 
+    data_hora_atual = datetime.now().strftime("%Y-%m-%d_%H-%M-%S") 
+    nome_arquivo_pdf = f"cadastro_relatorio_{data_hora_atual}.pdf" 
+    exportar(nome_arquivo_pdf, dados)
+
 
 if __name__ == "__main__":
     main()
